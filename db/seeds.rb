f13a7e84c1abfbe72911414bf938beb2f0c83812
Ira Sigman <irasigman@gmail.com>
2018-01-10 19:00:01 -0500

    production working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 User.create!(email: 'irasigman@gmail.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+AdminUser.create!(email: 'admin@csilongwood.com', password: 'csi_admin', password_confirmation: 'csi_admin') if Rails.env.production? 
