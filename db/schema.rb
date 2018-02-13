b68f0ae000f1345fca91b017dab1afb650cc3544
Ira Sigman <irasigman@gmail.com>
2018-02-13 18:31:55 -0500

    added tags migration

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20171219214750) do
     t.datetime "updated_at", null: false
   end
 
+create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
