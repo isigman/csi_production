e90990745bf2c539288f2f8147c1a1ec83889c12
Ira Sigman <irasigman@gmail.com>
2018-02-13 19:27:59 -0500

    add tags to schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20171219214750) do
     t.datetime "updated_at", null: false
   end
 
-create_table "tags", force: :cascade do |t|
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.integer "count"
     t.datetime "created_at", null: false
